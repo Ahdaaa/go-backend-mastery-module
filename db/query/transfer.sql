604a69ad44715226c167b7956fc9d55f4a566113
Ahdaaa <afilzag@gmail.com>
2024-05-10 22:32:09 +0700

    add: entry and transfer sqlc

---
+++ db/query/transfer.sql
@@ -0,0 +1,18 @@
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+  sender_account_id, 
+  receiver_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+ORDER BY id
+LIMIT $1
+OFFSET $2;
