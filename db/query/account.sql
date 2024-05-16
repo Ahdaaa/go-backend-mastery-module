460d013d54c7df5898c9da15b13be200814c625d
Ahdaaa <afilzag@gmail.com>
2024-05-16 22:37:38 +0700

    add: get account for update method

--- db/query/account.sql
+++ db/query/account.sql
@@ -11,6 +11,11 @@ INSERT INTO accounts (
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE; -- NO KEY will prevent deadlock transaction that is accessing foreign keys.
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id
