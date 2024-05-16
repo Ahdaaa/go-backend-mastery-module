86385e8491e8939b30e4c04689c22047b460447f
Ahdaaa <afilzag@gmail.com>
2024-05-16 22:39:49 +0700

    feat: testing transfer balance with goroutines

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,8 @@ package sqlc
 
 import (
 	"context"
+	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/Ahdaaa/go-backend-mastery-module/tree/main/util"
@@ -13,6 +15,7 @@ func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer tx (co-routines)
 	n := 5
@@ -24,8 +27,11 @@ func TestTransferTx(t *testing.T) {
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		// handle deadlock
+		txName := fmt.Sprintf("tx %d", i+1)
 		go func() { // go routines
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -78,7 +84,48 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
+		// get account results
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
 		// check accounts balance
-		// TODO!
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+		diff1 := new(big.Int)
+		diff1.Sub(account1.Balance.Int, fromAccount.Balance.Int)
+
+		diff2 := new(big.Int)
+		diff2.Sub(toAccount.Balance.Int, account2.Balance.Int)
+
+		modResult := new(big.Int)
+		modResult.Mod(diff1, amount.Int)
+
+		require.Equal(t, diff1, diff2)                     // they should be equal
+		require.True(t, diff1.Cmp(big.NewInt(0)) > 0)      // they should be positive
+		require.True(t, modResult.Cmp(big.NewInt(0)) == 0) // they should be divisible to the amount, because account balance will be decreased by 1*amount, 2*amount, till n*amount.
+
 	}
+
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	mulResult := new(big.Int)
+	mulResult.Mul(amount.Int, big.NewInt(int64(n))) // n * amount
+	subResult := new(big.Int)
+	subResult.Sub(account1.Balance.Int, mulResult)
+	sumResult := new(big.Int)
+	sumResult.Add(account2.Balance.Int, mulResult)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, subResult, updatedAccount1.Balance.Int)
+	require.Equal(t, sumResult, updatedAccount2.Balance.Int)
+
 }
